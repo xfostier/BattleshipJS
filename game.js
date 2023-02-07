dfd5019e32f5b2c821ca17584d6015b4997443de
Xavier Fostier <xavier.fostier@mailfence.com>
2023-02-07 21:47:55 +0100

    Set up JS head functions

--- game.js
+++ game.js
@@ -2,3 +2,15 @@ function squareClickHandler(id){
     let h = document.getElementById(id)
     h.setAttribute("style",'opacity:1; margin:1px; background-color: firebrick;')
 }
+
+function genGridWithElements(rowNumber, columnNumber, style, styleAdjustment = nil ,id = nil){
+
+}
+
+function genRowWithElements(numberOfElements, style, styleAdjustment = nil){
+
+}
+
+function genColumnWithElements(numberOfElements, style, styleAdjustment = nil){
+
+}
